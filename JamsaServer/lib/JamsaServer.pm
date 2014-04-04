e2f09a6bbdc3a446939ac8096b1534dc0872a041
Rafael Casado Sanchez <rafacas@gmail.com>
2014-04-04 17:36:37 +0200

    Returns the JSON version of the YAML file

--- JamsaServer/lib/JamsaServer.pm
+++ JamsaServer/lib/JamsaServer.pm
@@ -1,6 +1,9 @@
 package JamsaServer;
 use Dancer ':syntax';
 use Dancer::Serializer::JSON;
+use YAML;
+
+my $FILES_PATH="/home/jamsa/files";
 
 our $VERSION = '0.1';
 
@@ -12,7 +15,16 @@ get '/' => sub {
 
 get '/:id' => sub {
     my $id = params->{id};
-    return { id => $id, command => "speech", data => "" };
+    # Try to open the file $FILES_PATH/$id.yaml
+    my $status = 0;
+    open my $FILE, '<', "$FILES_PATH/$id.yaml" or $status = -1;
+    # Read the file
+    my $content = YAML::LoadFile($FILE);
+    # TODO: Check is a valid YAML file
+    # Convert YAML to JSON and return it
+    $content->{"id"} = $id;
+    $content->{"status"} = $status;
+    return ($content);
 };
 
 true;
